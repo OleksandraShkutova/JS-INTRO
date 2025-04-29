de0ad80d543399130b347ea002cf51fa30f62e76
Oleksandra <oleksandrashkutova@gmail.com>
2025-04-29 19:29:18 +0300

    Add js basics.

---
+++ assets/JS/index.js
@@ -0,0 +1,63 @@
+//alert('Hello, World');
+console.log('hi');
+
+//змінні
+/*let userName = 'test';
+console.log('userName :>> ', userName);
+console.log(1,2,3);
+userName = 13;
+console.log(userName);
+
+const constValue = 10;
+console.log(constValue);
+
+var anyValue = 7;
+console.log(anyValue);
+
+const bigintValue = 222222222222n;
+console.log(bigintValue);
+
+const isMarry = true;
+console.log(isMarry);
+
+const nullValue = null;
+console.log(nullValue);
+
+console.log('tyoeof 10:', typeof 10);
+console.log('typeof anyValue:', typeof anyValue);
+
+console.log('11n:', typeof 11n);
+console.log('"11":', typeof "11");
+console.log('true:', typeof true);
+console.log('"true":', typeof "true");
+console.log('null:', typeof null);
+console.log(typeof objValue);
+
+const userFirstName = "Sasha";
+let userOld = 37;
+console.log(userFirstName, userOld);
+console.log('userFirstName:', typeof userFirstName);
+console.log('userOld:', typeof userOld);
+console.log(typeof null);
+
+const greeting = "You are ";
+const age = prompt("How old are you?");
+console.log(age);
+console.log(greeting + age + " years old");
+
+let b = 10;
+b += 22;
+console.log(b);
+*/
+
+let c = +prompt('Add first numder:');
+let d = +prompt('Add second number:');
+let summa = c + d;
+console.log(summa);
+
+let dil = c / d;
+console.log(dil);
+
+
+
+
