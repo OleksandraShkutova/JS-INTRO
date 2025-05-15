3232c239d9d44c65189b69e82f367553f8cd4e6b
Oleksandra <oleksandrashkutova@gmail.com>
2025-05-15 15:53:27 +0300

    Add reference while, do .. while, for.

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,106 +1,58 @@
-/*let isDay = true;
-if (isDay) {
-    console.log('Світла тема');
-} else {
-    console.log('Темна тема');
+
+/*let a = 1;
+let b = +prompt('Скільки сторінок?');
+while (a <= b) {
+    if (a % 2 === 0){
+        console.log('Номер сторінки:', a);
+    }
+    a = a + 1;
 }
 
-let isOnline = false;
-if (!isOnline) {
-    console.log('Ваше повідомлення буде відправлено, коли буде інтернет');
-}*/
+let i = 1;
+let summ = 0;
+while(i <= 5){
+    summ = summ + i;
+    i = i + 1;
+}
+console.log(summ);
 
-let summ = Number(prompt('Введіть суму покупки  '));
-if (summ < 500) {
-    console.log('Сума покупки = ', summ);
-} if (summ < 800 && summ >= 500 ) {
-    console.log('Сума покупки = ', summ * 0.97);
-} if (summ >= 800) {
-    console.log('Сума покупки = ', summ * 0.95 );
+let i = 1;
+let mult = 1;
+while(i <= 5) {
+    if (i % 2 === 0){
+      mult = mult * i;  
+    }
+    i = i + 1;
+}
+console.log(mult);
+
+let i = 1;
+let mult = 1;
+while(i <= 5) {
+    mult = mult * i;  
+    i++
+}
+console.log(mult);
+
+for ( let i = 10; i <= 100; i = i + 10){
+    console.log(i);
 }
 
-/*let day = +prompt('Введіть день тижня');
-switch (day) {
-    case 1:
-        console.log('Понеділок');
-        break;
-    case 2:
-        console.log('Вівторок');
-        break;
-    case 3:
-        console.log('Середа');
-        break;
-    case 4:
-        console.log('Четвер');
-        break;
-    case 5:
-        console.log('П"ятниця');
-        break;
-    case 6:
-        console.log('Субота');
-        break;
-    case 7:
-        console.log('Неділя');
-        break;       
-    default:
-        console.log('Такого дня не існує!');
-} 
+let sum = 0;
+for ( let i = 3; i <= 11; i = i + 2){
+    sum = sum + i;
+}
 
+console.log(sum / 5);
 
-let day = +prompt('Введіть день місяця');
-switch (day) {
-    case 1: 
-    case 2:
-    case 3:
-    case 4:
-    case 5:
-    case 6:
-    case 7:
-    case 8:
-    case 9:
-    case 10:
-        console.log('Перша декада');
-        break;
-    case 11: 
-    case 12:
-    case 13:        
-    case 14:
-    case 15:
-    case 16:
-    case 17:
-    case 18:
-    case 19:
-    case 20:
-        console.log('Друга декада');
-        break;
-    case 21: 
-    case 22:
-    case 23:        
-    case 24:
-    case 25:
-    case 26:
-    case 27:
-    case 28:
-    case 29:
-    case 30:
-    case 31:
-        console.log('Третя декада');
-        break;       
-    default:
-        console.log('Не може бути!');
-} */
+let d = 1;
+for (let i = 10; i >= -5; i = i - 1){
+    d = d * i;
+}
+console.log(d);
 
-
-
-
-
-
-
-
-
-
-
-
-
+let greeting = 'Hello';
+let userName = prompt('Username?');
+console.log(`${greeting}, ${userName}!`);*/
 
 
