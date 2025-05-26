2b726570c803075dedf586db4a7e40727f187da7
Oleksandra <oleksandrashkutova@gmail.com>
2025-05-26 21:35:48 +0300

    Add object.

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,40 +1,122 @@
 
-// 1
-function isWorkingAgePerson(age){
-    return age >= 20 && age < 88;
+/*let user = {
+    firstName: 'Vika',
+    lastName: 'Lyn',
+    age: 7,
 }
-console.log(isWorkingAgePerson(20));
-console.log(isWorkingAgePerson(4));
-console.log(isWorkingAgePerson(88));
-console.log(isWorkingAgePerson(66));
+console.log('Object', user);
 
-// 2
-function checkMultiplicity(a, b){
-    return a % b === 0;
+let userName = user.firstName;
+console.log(userName);
+
+
+let car = {
+    brend: 'Mercedes-Benz',
+    model: 'SLS AMG',
+    made: 2023,
+    number: 'BK1313ZE',
+    color: 'grafit',
+    //вкладені обєкти
+    param: {
+        height: 1.2,
+        weight: 2.0,
+    } 
+    ,
+    //функція
+    changeColor(newColor){
+        this.color = newColor;
+    },
 }
-console.log(checkMultiplicity(25, 5));
-console.log(checkMultiplicity(15, 3));
-console.log(checkMultiplicity(15, 5));
-console.log(checkMultiplicity(15, 4));
 
-//3
-function isTriangle(a, b, c) {
-    return a + b > c || a + c > b || b + c > a && (a > 0, b > 0, c > 0);
+car.changeColor('red');
+console.log('User car:', car);
+
+
+
+/*
+//змінюємо
+car.color = 'blue';
+console.log('User car:', car);
+
+//видаляємо
+delete car.model;
+console.log('User car:', car);
+
+//додаємо
+car.user = 'Sasha Sonce';
+console.log('User car:', car);
+
+//переглядаємо зі змінами
+console.log('User car:', car);
+
+
+//копіювання
+let car2 = Object.assign({}, car);
+car2.color = 'white',
+console.log('car2', car2);
+
+let car3 = {...car};
+car3.color = 'black',
+console.log('car3', car3);
+
+console.log(car === car2);
+console.log(car === car3);
+
+
+for(let key in car){
+    console.log(`car: ${key} = ${car[key]}`);
 }
-console.log(isTriangle(2, 2, 3)); 
 
-//повинна виконуватись ще умова більшої сторони, але я незнаю як її прописати.
-//Багото і або
+//перегляд обєкта
+console.dir(car);
 
-// 4
-function s(a, b = a){
-    return a * b;
-} 
-console.log(s(2,3));
-console.log(s(4));
+//перегляд функції як обєкта
+console.dir(function a(b,c){});
 
-// 5
-function lightNumber(a){
-    return (a % a === 0 && a % 2 === 1);
+
+
+const car1 = {
+	brand: 'Volkswagen',
+};
+
+const car2 = {
+	brand: 'Volkswagen',
+};
+
+console.log(car1 === car2);
+*/
+
+function UserCar(brend, model, made, number,color){
+    this.brend = brend;
+    this.model = model;
+    this.made = made;
+    this.number = number;
+    this.color = color;
 }
-console.log(lightNumber(+prompt('введіть число')));
+
+//задаємо обєкт    
+let carProto = {};
+
+//прописуємо метод в створений обєкт
+carProto.changeNumber = function(newNumber){
+    this.number = newNumber;
+}
+
+//призначаємо створений обєкт в властивість prototype функції-конструктора
+UserCar.prototype = carProto;
+
+
+let userCar1 = new UserCar('Mercedes-Benz', 'SLS AMG', 2023, 'BK1313ZE', 'grafit');
+console.log(userCar1);
+
+let userCar2 = new UserCar('Audi', 'Q6', 2014, 'BK0101ZE', 'red');
+console.log(userCar2);
+
+UserCar.prototype.changeNumber = prompt('введіть номер авто');
+
+
+
+
+
+
+
