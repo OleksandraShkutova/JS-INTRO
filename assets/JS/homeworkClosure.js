4bbabbb275f3d3c319611dc1cb785cb283c7bbb2
Oleksandra <oleksandrashkutova@gmail.com>
2025-07-09 15:55:05 +0300

    Add change to homework 2-08.

--- assets/JS/homeworkClosure.js
+++ assets/JS/homeworkClosure.js
@@ -14,7 +14,7 @@ const myCounter = counter();
 console.log('result1:', myCounter());
 console.log('result2:', myCounter());
 console.log('result3:', myCounter());
-*/
+
 
 // 2
 function counter () {
@@ -31,3 +31,38 @@ const myCounter2 = counter();
 console.log('result1:', myCounter2());
 console.log('result2:', myCounter2());
 console.log('result3:', myCounter2());
+
+
+// or that
+// 1
+function counter (step) {
+    let i = 0
+    return function () {
+        return i = i + step
+    }
+}
+
+const step = +prompt('введіть число');
+
+const myCounter = counter(step);
+console.log('result1:', myCounter());
+console.log('result2:', myCounter());
+console.log('result3:', myCounter());
+*/
+
+// 2
+
+function counter (i , n) {
+    return function () {
+        return i = i + n
+    }
+}
+
+const i = +prompt('введіть початкове значення');
+const n = +prompt('введіть число');
+
+const myCounter = counter(i , n);
+console.log('result1:', myCounter());
+console.log('result2:', myCounter());
+console.log('result3:', myCounter());
+
